62cf2b0b193cbd505267b8e68d3502f34c006812
Yuliya Belyavskaya <JuliyaD152@yandex.by>
2020-10-10 20:48:43 +0200

    docs: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = {};
+  bracketsConfig.forEach((item) =>{
+    brackets[item[1]] = item[0];
+  });
+  let newArr= [];
+  for (let i = 0; i<str.length; i++){
+      if(i === 0){
+          newArr.push(str[0]);
+      }else{
+          if(newArr.length !=0 && newArr[newArr.length -1] == brackets[str[i]]){
+              newArr.pop();
+          }else {
+              newArr.push(str[i]);
+          }
+      }
+  }
+  return newArr.length == 0;
+};
